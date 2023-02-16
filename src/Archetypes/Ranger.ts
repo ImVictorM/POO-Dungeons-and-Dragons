6fa7cf6d950672f5660a5bd5346cde019791324d
Victor Mendes <victorutdsim7@gmail.com>
2023-02-16 18:22:07 -0300

    [Req 5]feat: cria arquétipo de ranger

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  private static _createdInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger._createdInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static override createdArchetypeInstances(): number {
+    return Ranger._createdInstances;
+  }
+}
+
+export default Ranger;
