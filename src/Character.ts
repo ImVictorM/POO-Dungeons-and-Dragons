fcc084e572cfe3f3703e31a11156f6001b875328
victor <victorutdsim7@gmail.com>
2023-02-16 20:28:32 -0300

    [Req 7]feat: implementa metodos da classe Character

--- src/Character.ts
+++ src/Character.ts
@@ -34,20 +34,72 @@ class Character implements Fighter {
     return this._race;
   }
 
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy | undefined {
+    if (this._energy) {
+      return {
+        amount: this._energy.amount,
+        type_: this._energy.type_
+      }
+    }
+    return undefined;
+  }
+
   attack(enemy: Fighter): void {
-    throw new Error('Method not implemented.');
+    enemy.receiveDamage(this._strength);
   }
 
   special?(enemy: Fighter): void {
-    throw new Error('Method not implemented.');
+    if (this.energy && this.energy.amount >= 9) {
+      enemy.receiveDamage(this.strength * getRandomInt(1, 4));
+    }
+    console.log('You don\'t have enough mana');
   }
 
   levelUp(): void {
-    throw new Error('Method not implemented.');
+    const attrToIncrease = [this._maxLifePoints, this._strength, this._dexterity, this._defense];
+    // increasing character attributes
+    attrToIncrease.forEach((attr) => attr += getRandomInt(1, 10));
+    if (this._maxLifePoints > this.race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints; // reset life
+    if (this._energy) {
+      // reset energy amount
+      this._energy.amount = 10;
+    }
   }
 
   receiveDamage(attackPoints: number): number {
-    throw new Error('Method not implemented.');
+    const receivedDamage = attackPoints - this._defense;
+    if (receivedDamage > 0) {
+      this._lifePoints -= receivedDamage;
+    } else {
+      this._lifePoints -= 1;
+    }
+    if (this._lifePoints < 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
   }
 }
 
